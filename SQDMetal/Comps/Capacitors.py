c590104976bfe220fdf3acea9a3ee775c37d6468
Prasanna Pakkiam <p.pakkiam@uq.edu.au>
2023-09-06 02:57:39 +1000

    Added a CapacitorGapPinPin.

--- SQDMetal/Comps/Capacitors.py
+++ SQDMetal/Comps/Capacitors.py
@@ -689,7 +689,7 @@ class CapacitorGap(QComponent):
         self.add_pin('b', pin2.coords[::-1], width=p.cpw_width) 
     
     @staticmethod
-    def _draw_capacitor(p, design):
+    def _draw_capacitor(p, design, discard_leads=False):
         # Make the shapely polygons for the main cap structure
         len_trace = np.sqrt((p.end_x-p.pos_x)**2+(p.end_y-p.pos_y)**2)
         pad1 = [(0, p.cpw_width*0.5),
@@ -755,6 +755,14 @@ class CapacitorGap(QComponent):
                       (len_trace*0.5+p.cap_gap*0.5, p.cap_width*0.5+gap_cpw_cap),
                       (len_trace*0.5-p.cap_gap*0.5, p.cap_width*0.5+gap_cpw_cap)]
         polyBndMid = shapely.Polygon(polyBndMid)
+
+        if discard_leads:   #TODO: Could write this to be a little less lazy?
+            pad1 = pad1[2:-1,:]
+            pad2 = pad2[1:-2,:]
+            padGap1 = padGap1[2:-1,:]
+            padGap2 = padGap2[1:-2,:]
+            pin1 = pad1[[-1,0],:]
+            pin2 = pad2[[-1,0],:]
 
         pad1 = shapely.Polygon(pad1)
         pad2 = shapely.Polygon(pad2)
@@ -882,6 +890,143 @@ class CapacitorGapPinStretch(QComponent):
         p.end_y = endPt[1]
 
         pad1, pad2, padGap1, padGap2, pin1, pin2, polyBndMid = CapacitorGap._draw_capacitor(p, self._design)
+
+        self.bound_poly_mid = polyBndMid
+
+        # Adds the object to the qgeometry table
+        self.add_qgeometry('poly',
+                           dict(pad1=pad1,
+                                pad2=pad2),
+                           layer=p.layer)
+
+        #subtracts out ground plane on the layer it's on
+        self.add_qgeometry('poly',
+                           dict(padGap1=padGap1,
+                                padGap2=padGap2),
+                           subtract=True,
+                           layer=p.layer)
+
+        # Generates its own pins
+        self.add_pin('a', pin1.coords[::-1], width=p.cpw_width)
+        self.add_pin('b', pin2.coords[::-1], width=p.cpw_width) 
+
+class CapacitorGapPinPin(QComponent):
+    """Creates a gap capacitor on a CPW with an optional bisectional ground plane.
+
+    Inherits QComponent class.
+
+    Capacitor Metal Geometry and Ground Cutout Pocket:
+        * len_diag - Length of the staggered region that bridges from CPW to the capacitor
+        * len_flat - Length of the flat region before starting onto the fingers
+        * cap_width - Width of the main capacitor
+        * cap_gap  - Distance between the two conductors of the capacitor
+        * gnd_width - Width of ground plane that bisects the two conductors of the capacitor (can be zero)
+        * offset_lead1 - Offsets the first lead (positive being to the right when facing into the capacitor) along the capacitor.
+        * offset_lead2 - Offsets the second lead (positive being to the right when facing into the capacitor) along the capacitor.
+
+    The spacing (i.e. cuts into the ground plane) can be controlled via:
+        * side_gap - If this is zero, then the gap on the sides of the capacitor is calculated via a 50ohm impedance CPW line. Otherwise,
+                     it is set via the given side gap.
+        * init_pad - This adds spacing to the ground plane on the feed lines. This is useful when the diagonal section is steep (e.g.
+                     when len_diag is zero) to ensure that the ground plane does not intersect with the main capacitor conductors. The
+                     ground plane spacing typically starts to change on meeting with the LD or LF sections (see below). If init_pad > 0,
+                     it starts from an earlier point.
+
+    The positioning can be done dynamically via:
+        * pin_inputs=Dict(start_pin=Dict(component=f'...',pin='...'), end_pin=Dict(component=f'...',pin='...')) - Specifying start and end
+          positions via a component pins
+    The resulting capacitor is right in the centre. This class ignores pos_x, pos_y and orientation...
+
+    Pins:
+        There are two pins on the capacitor at either end
+        The pins attach directly to the built in lead length and only needs a width defined
+        * trace_width - center trace width of the trace lead line and cap fingers
+
+    Note that the pins are not drawn to ensure good compatibility with routing/wiring constructs.
+
+    Sketch:
+        Below is a sketch of the capacitor
+        ::
+
+            @@@@   |   @@@@                                         When setting len_diag=0:
+            @@@@   |   @@@@            @   = Ground Plane               @@@@@   |   @@@@@
+            @@@@   |   @@@@            -,| = Conductors                 @@@@@   |   @@@@@
+            @@@@   -    @@@ LD                                          @@      |   IP @@
+            @@@   ---    @@ LD         LD = len_diag                    @      ---  LD  @
+            @@   -----    @ LD                                          @    -------LF  @
+            @   -------   @ LF         LF = len_flat                    @    -------LF  @     IP = init_pad
+            @   -------   @ LF                                          @           CG  @
+            @   WWWWWWW   @ CG         W = cap_width                    @           CG  @
+            @             @ CG                                       GW @@@@@@@@@@@@@@@@@
+         GW @@@@@@@@@@@@@@@ CG         CG = cap_gap                     @           CG  @
+         GW @@@@@@@@@@@@@@@ CG         GW = gnd_width                   @           CG  @
+         GW @@@@@@@@@@@@@@@ CG                                          @    -------LF  @
+            @             @ CG                                          @    -------LF  @
+            @             @ CG                                          @      ---  LD  @
+            @   -------   @ LF                                          @@      |   IP @@
+            @   -------   @ LF                                          @@@@@   |   @@@@@
+            @@   -----    @ LD                                          @@@@@   |   @@@@@
+            @@@   ---    @@ LD
+            @@@@   -    @@@ LD
+            @@@@   |   @@@@
+            @@@@   |   @@@@
+            @@@@   |   @@@@
+
+    .. image::
+        Cap3Interdigital.png
+
+    .. meta::
+        Cap 3 Interdigital
+
+    Default Options:
+        * trace_width: '10um
+        * pos_x='0um',pos_y='0um'
+        * end_x='50um',end_y='0um'
+        * len_diag='5um'
+        * len_flat='5um'
+        * cap_gap='3um'
+        * gnd_width='1um'
+        * side_gap='0um'
+        * init_pad='0um'
+        * offset_lead1='0um'
+        * offset_lead2='0um'
+    """
+
+    #  Define structure functions
+
+    default_options = Dict(pos_x='0um',pos_y='0um',
+                           end_x='50um',end_y='0um',
+                           cpw_width='10um',
+                           cap_width='20um',
+                           len_diag='5um',
+                           len_flat='5um',
+                           cap_gap='3um',
+                           gnd_width='1um',
+                           side_gap='0um',
+                           init_pad='0um',
+                           offset_lead1='0um',
+                           offset_lead2='0um')
+    """Default drawing options"""
+
+    TOOLTIP = """Create a three finger planar capacitor with a ground pocket cuttout."""
+
+    def make(self):
+        """This is executed by the user to generate the qgeometry for the
+        component."""
+        p = self.p
+        #########################################################
+
+        start_point = self.design.components[self.options.pin_inputs.start_pin.component].pins[self.options.pin_inputs.start_pin.pin]
+        startPt = start_point['middle']
+        p.pos_x = startPt[0]
+        p.pos_y = startPt[1]
+
+        end_point = self.design.components[self.options.pin_inputs.end_pin.component].pins[self.options.pin_inputs.end_pin.pin]
+        endPt = end_point['middle']
+        p.end_x = endPt[0]
+        p.end_y = endPt[1]
+
+        pad1, pad2, padGap1, padGap2, pin1, pin2, polyBndMid = CapacitorGap._draw_capacitor(p, self._design, True)
 
         self.bound_poly_mid = polyBndMid
 
