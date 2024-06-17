48638dae4e86ee6c633cec8e73dcfe04d5559392
Prasanna Pakkiam <p.pakkiam@uq.edu.au>
2024-06-17 14:26:04 +1000

    Added rounding post-processing to the GDS exporter.

--- SQDMetal/Utilities/MakeGDS.py
+++ SQDMetal/Utilities/MakeGDS.py
@@ -6,7 +6,7 @@ from SQDMetal.Utilities.QiskitShapelyRenderer import QiskitShapelyRenderer
 from SQDMetal.Utilities.ShapelyEx import ShapelyEx
 
 class MakeGDS:
-    def __init__(self, design, threshold=1e-9, precision=1e-9, curve_resolution=22):
+    def __init__(self, design, threshold=1e-9, precision=1e-9, curve_resolution=22, smooth_radius=0):
         '''
         Inputs:
             design      - The QiskitMetal design object...
@@ -19,7 +19,37 @@ class MakeGDS:
         self.curve_resolution = curve_resolution
         self.threshold = threshold
         self.precision = precision
+        self.smooth_radius = smooth_radius
+        self.gds_units = 1e-6
         self.refresh()
+
+    def _shapely_to_gds(self, metals, layer):
+        if isinstance(metals, shapely.geometry.multipolygon.MultiPolygon):
+            temp_cur_metals = [x for x in metals.geoms]
+        else:
+            temp_cur_metals = [metals]
+        gds_metal = None
+        for cur_poly in temp_cur_metals:
+            if gds_metal:
+                gds_metal = gdspy.boolean(gdspy.Polygon(cur_poly.exterior.coords[:]), gds_metal, "or", layer=layer, max_points=0)
+            else:
+                gds_metal = gdspy.Polygon(cur_poly.exterior.coords[:], layer=layer)
+            for cur_int in cur_poly.interiors:
+                gds_metal = gdspy.boolean(gds_metal,gdspy.Polygon(cur_int.coords[:]), "not", layer=layer, max_points=0)
+        return gds_metal
+
+    def _round_corners(self, gds_metal, output_layer):
+        #Perform the usual expand and contract to join fractures... But this fails due to some gdsPy bugs?
+        # new_metal = gdspy.boolean(gdspy.offset(new_metal, self.threshold/self.gds_units, max_points=0, join_first=True), None, "or", max_points=0)
+        # new_metal = gdspy.boolean(gdspy.offset(new_metal, -self.threshold/self.gds_units, max_points=0, join_first=True), None, "or", max_points=0)
+        # new_metal = new_metal.fillet(self.smooth_radius/self.gds_units, max_points=0)
+
+        #So use Shapely instead...
+        polyFuse = ShapelyEx.fuse_polygons_threshold([shapely.Polygon(x) for x in gds_metal.polygons], self.threshold/self.gds_units)
+        rndDist = self.smooth_radius/self.gds_units
+        polyFuse = polyFuse.buffer(rndDist*0.5, join_style=1, quad_segs=self.curve_resolution).buffer(-rndDist, join_style=1).buffer(rndDist*0.5, join_style=1, quad_segs=self.curve_resolution)
+        new_metal = self._shapely_to_gds(polyFuse, output_layer)
+        return new_metal
 
     def refresh(self):
         '''
@@ -37,7 +67,6 @@ class MakeGDS:
         self.cell = self.lib.new_cell('Main')
 
         threshold = self.threshold
-        gds_units = 1e-6
 
         #Assemble geometry for each layer
         all_layers = []
@@ -52,7 +81,7 @@ class MakeGDS:
         #
         rect = shapely.Polygon([(cx-0.5*sx, cy-0.5*sy), (cx+0.5*sx, cy-0.5*sy), (cx+0.5*sx, cy+0.5*sy), (cx-0.5*sx, cy+0.5*sy), (cx-0.5*sx, cy-0.5*sy)])
         chip = rect.difference(gaps)
-        chip = shapely.affinity.scale(chip, xfact=leUnits/gds_units, yfact=leUnits/gds_units, origin=(0,0))
+        chip = shapely.affinity.scale(chip, xfact=leUnits/self.gds_units, yfact=leUnits/self.gds_units, origin=(0,0))
         all_layers.append((0, chip))
         #
         #
@@ -60,30 +89,21 @@ class MakeGDS:
         for layer in leLayers:
             metals = gsdf.loc[(gsdf['layer'] == layer) & (gsdf['subtract'] == False)]
             metals = ShapelyEx.fuse_polygons_threshold(metals['geometry'].tolist(), threshold)
-            metals = shapely.affinity.scale(metals, xfact=leUnits/gds_units, yfact=leUnits/gds_units, origin=(0,0))
+            metals = shapely.affinity.scale(metals, xfact=leUnits/self.gds_units, yfact=leUnits/self.gds_units, origin=(0,0))
             all_layers.append((layer, metals))
 
         neg_layer_offset = np.max(leLayers)+10
         for cur_layer in all_layers:
             layer, metals = cur_layer
 
-            if isinstance(metals, shapely.geometry.multipolygon.MultiPolygon):
-                temp_cur_metals = [x for x in metals.geoms]
-            else:
-                temp_cur_metals = [metals]
-            gds_metal = None
-            for cur_poly in temp_cur_metals:
-                if gds_metal:
-                    gds_metal = gdspy.boolean(gdspy.Polygon(cur_poly.exterior.coords[:]), gds_metal, "or", layer=layer, max_points=1000000)
-                else:
-                    gds_metal = gdspy.Polygon(cur_poly.exterior.coords[:], layer=layer)
-                for cur_int in cur_poly.interiors:
-                    gds_metal = gdspy.boolean(gds_metal,gdspy.Polygon(cur_int.coords[:]), "not", layer=layer, max_points=1000000)
+            gds_metal = self._shapely_to_gds(metals, layer)
 
-            gds_rect_neg = gdspy.boolean(gdspy.Rectangle(((cx-0.5*sx)*leUnits/gds_units, (cy-0.5*sy)*leUnits/gds_units),
-                                                        ((cx+0.5*sx)*leUnits/gds_units, (cy+0.5*sy)*leUnits/gds_units)),
-                                        gds_metal, "not", layer=layer+neg_layer_offset, max_points=1000000)
+            gds_rect_neg = gdspy.boolean(gdspy.Rectangle(((cx-0.5*sx)*leUnits/self.gds_units, (cy-0.5*sy)*leUnits/self.gds_units),
+                                                        ((cx+0.5*sx)*leUnits/self.gds_units, (cy+0.5*sy)*leUnits/self.gds_units)),
+                                        gds_metal, "not", layer=layer+neg_layer_offset, max_points=0)
 
+            if self.smooth_radius > 0:
+                gds_metal = self._round_corners(gds_metal, layer)
             self.cell.add(gdspy.boolean(gds_metal, gds_metal, "or", layer=layer))    #max_points = 199 here...
             self._layer_metals[layer] = gds_metal
             if gds_rect_neg:
@@ -95,9 +115,13 @@ class MakeGDS:
         if output_layer == None:
             output_layer = max([x for x in self._layer_metals]) + 1
 
-        new_metal = gdspy.boolean(self._layer_metals[layer1_ind], self._layer_metals[layer2_ind], operation, layer=output_layer, precision=1e-6, max_points=1000000)
+        new_metal = gdspy.boolean(self._layer_metals[layer1_ind], self._layer_metals[layer2_ind], operation, layer=output_layer, precision=1e-6, max_points=0)
+        if self.smooth_radius > 0:
+            new_metal = self._round_corners(new_metal, output_layer)
 
-        self.cell.add(gdspy.boolean(new_metal, new_metal, "or", layer=output_layer))    #max_points = 199 here...
+        new_metal = gdspy.boolean(new_metal, new_metal, "or", layer=output_layer)    #max_points = 199 here...
+
+        self.cell.add(new_metal)
         self._layer_metals[output_layer] = new_metal
 
     def export(self, file_name):
--- docs/User/GDS.md
+++ docs/User/GDS.md
@@ -18,4 +18,19 @@ new_layer_ind = leGDS.add_boolean_layer(0, 1, "or")
 leGDS.export('first.gds')
 ```
 
-Note that the returned layer index can be used for further boolean operations.
+Note that the returned layer index can be used for further boolean operations. The geometry can also be rounded in post-processing:
+
+```python
+from SQDMetal.Utilities.MakeGDS import MakeGDS
+
+#Set the initial components to be with no rounding - can set it to have some rounding if desired (in units of metres)
+leGDS = MakeGDS(design, curve_resolution=60, precision=1e-12, smooth_radius=0)    #Use 60pts per quarter rotation and ensure enough precision to have nice rounded corners
+
+#Set the rounding radius to be non-zero now... Now all subsequent layers will have a rounding of 200nm
+leGDS.smooth_radius = 200e-9
+bottom_layer = gdsexp.add_boolean_layer(0,1,'or')
+top_layer = gdsexp.add_boolean_layer(2,2,'or')
+
+leGDS.export('rounded.gds')
+```
+
