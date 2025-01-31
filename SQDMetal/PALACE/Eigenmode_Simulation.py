644038cd14ba666859b0e2f5d61057e0ba67931d
David Sommers <david.sommers300@gmail.com>
2025-01-31 14:02:43 +1000

    code for EPR calculations completed

--- SQDMetal/PALACE/Eigenmode_Simulation.py
+++ SQDMetal/PALACE/Eigenmode_Simulation.py
@@ -308,3 +308,119 @@ class PALACE_Eigenmode_Simulation(PALACE_Model_RF_Base):
         col_Ref = [x for x in range(len(headers)) if headers[x].strip().startswith(r'Re{f}')][0]
 
         return {'mat_mode_port': raw_data[:,1:], 'eigenfrequencies': raw_dataE[:,col_Ref]*1e9}
+
+    @staticmethod
+    def calculate_hamiltonian_parameters_EPR(directory, modes_to_compare = []):
+        
+        mode_dict = PALACE_Eigenmode_Simulation.retrieve_mode_port_EPR_from_file(directory)
+        
+        #Constants used for calculations
+        e = 1.60218e-19
+        hbar = 1.05457e-34
+        phi0 = hbar/(2*e)
+
+        #get particpation ratios and frequencies
+        participation_ratios = mode_dict['mat_mode_port']
+        frequencies = mode_dict['eigenfrequencies']
+
+        #get #modes and #junctions from arrays
+        num_of_modes = np.shape(participation_ratios)[0]
+        num_of_junctions = np.shape(participation_ratios)[1]
+
+        #choose modes user is interested in
+        modes = np.arange(num_of_modes) + 1 # array of modes
+        modes_to_compare = np.array(modes_to_compare) #cast to array
+        if modes_to_compare.size == 0:
+            modes_to_compare = modes
+        modes_to_delete = np.setdiff1d(modes, modes_to_compare)
+
+        #update number of modes left for comparison
+        num_of_modes = num_of_modes - modes_to_delete.size
+
+        #create labels for the modes
+        dataframe_label_mode = []
+        for i, mode in enumerate(modes_to_compare):
+            dataframe_label_mode.append("mode_" + str(mode))
+
+        dataframe_label_junc = []
+        for i in range(num_of_junctions):
+            dataframe_label_junc.append("jj_" + str(i+1))
+
+        #delete modes not wanted from participation ratio and frequency vectors
+        if modes_to_delete.size > 0:
+            for i,mode in enumerate(modes_to_delete):
+                participation_ratios = np.delete(participation_ratios, (modes_to_delete[i] - (i+1)), axis=0)
+                frequencies = np.delete(frequencies, (modes_to_delete[i] - (i+1)), axis=0)
+
+        #participation ratio matrix - dimension (num_of_modes, num_of_junctions)
+        P = np.matrix(np.abs(participation_ratios))
+
+        #diagonal matrix for eigenfrequencies
+        omega_vector = 2* np.pi * frequencies
+        omega = np.diag(omega_vector)
+
+        #Detuning matrix
+        delta = np.ones((np.shape(omega)[0], np.shape(omega)[0])) #create delta/detuning matrix
+
+        #Populate delta/detuning matrix
+        for j in range(np.shape(omega)[0]):
+            delta[:,j] = omega_vector - omega_vector[j]
+
+        #Get Ej for each junction
+        Lj = np.matrix([[13e-9], [9e-9]]) ##### need function to extract this #####
+        Ej = np.diag((phi0**2 / Lj).A1)
+
+        #Calcultate Kerr matrix: chi = hbar/4 * (omega*P) * inv_Ej * (omega*P)^T
+        ###Please note all these values are angular frequencies radians/sec, to get frequencies in Hetz divide by 2*pi###
+        chi = (hbar/4) * (omega*P) * np.linalg.inv(Ej) * np.transpose(omega*P)
+        diag_matrix = (-1/2) * np.diag(np.ones(num_of_modes)) + np.ones(num_of_modes) #hacky way of creating ones matrix with values of 1/2 down the diagonal
+        chi_anharm = np.multiply(diag_matrix,chi) #This is the kerr matrix but with the anharmonicity down the diagonal for the self-kerr term
+        anharm = (1/2) * np.diagonal(chi)
+        lamb_shift = (1/2) * chi * np.ones((np.shape(chi)[0],1))
+        renormalised_freqs = np.transpose(np.matrix(frequencies * 2 * np.pi)) - lamb_shift #linear frequencies from eigenmode simulation are renormalised by lamb shift caused by non-linearity
+
+        #Get values in mehahertz (MHz) to display to user
+        chi_freq = chi / (2 * np.pi * 1e6) 
+        chi_anharm = chi_anharm / (2 * np.pi * 1e6) 
+        anharm_freq = anharm / (2 * np.pi * 1e6)
+        lamb_shift_freq = lamb_shift / (2 * np.pi * 1e6)
+        delta_freq = delta / (2 * np.pi * 1e9)
+        renormalised_freqs = renormalised_freqs /  (2 * np.pi * 1e9)
+
+        #create dataframes and print for user to see
+        print('Simulation Mode Frequencies:')
+        freq_df = pd.DataFrame(frequencies / (1e9), dataframe_label_mode, ['Freq (GHz)'])
+        print(freq_df)
+        print('______________________________')
+        print('\n')
+
+        print('Renormalised Frequencies:\n ***Freqs from simulation are adjusted for lamb shift')
+        freq_renorm_df = pd.DataFrame(renormalised_freqs, dataframe_label_mode, ['Freq (GHz)'])
+        print(freq_renorm_df)
+        print('______________________________')
+        print('\n')
+
+        print('Participation Ratios:')
+        ratios_df = pd.DataFrame(np.abs(participation_ratios), dataframe_label_mode, dataframe_label_junc)
+        print(ratios_df)
+        print('______________________________')
+        print('\n')
+
+        chi_anharm_df = pd.DataFrame(chi_anharm, dataframe_label_mode, dataframe_label_mode)
+        print('Chi Matrix (MHz):\n ***Diag is Anharmonicity, Off-Diag is Cross-Kerr')
+        print(chi_anharm_df)
+        print('______________________________')
+        print('\n')
+
+        lamb_shift_df = pd.DataFrame(lamb_shift_freq, dataframe_label_mode, ['Lamb Shift (MHz)'])
+        print('Lamb Shifts:')
+        print(lamb_shift_df)
+        print('______________________________')
+        print('\n')
+
+        delta_df = pd.DataFrame(delta_freq, dataframe_label_mode, dataframe_label_mode)
+        print('Detuning (GHz):')
+        print(delta_df)
+        print('______________________________')
+        print('\n')
+
