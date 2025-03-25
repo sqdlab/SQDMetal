45b52fb18c6f42249423cd5d0ec068974989fddc
Prasanna Pakkiam <p.pakkiam@uq.edu.au>
2025-03-25 17:10:48 +1000

    Fixed several bugs in U-clip and conformal meshing.

--- SQDMetal/PALACE/Utilities/GMSH_Geometry_Builder.py
+++ SQDMetal/PALACE/Utilities/GMSH_Geometry_Builder.py
@@ -71,8 +71,6 @@ class GMSH_Geometry_Builder:
         metal_list = self._create_gmsh_geometry_from_shapely_polygons(metals)                           #create all metal traces
         dielectric_gap_list = self._create_gmsh_geometry_from_shapely_polygons(dielectric_gaps)         #create all the gaps between the metal traces and the ground plane 
 
-
-
         #Create the chip base (dielectric substrate) in Gmsh
         chip_base = self._create_chip_base()
         
@@ -109,11 +107,10 @@ class GMSH_Geometry_Builder:
             gmsh.model.occ.synchronize()
             gmsh.model.geo.synchronize()
             #
-            lePortPoly = gmsh.model.addPhysicalGroup(2, [x[1] for x in sim_poly], name = cur_sim_poly[0])
             sim_poly_list = [(2,x[1]) for x in sim_poly]
             fragment_list += sim_poly_list
             aux_list += sim_poly_list
-            lePortPolys[cur_sim_poly[0]] = lePortPoly
+            lePortPolys[cur_sim_poly[0]] = [x[1] for x in sim_poly]
 
         #list for metals for capacitance simulation
         metal_cap_physical_group = []
@@ -130,13 +127,38 @@ class GMSH_Geometry_Builder:
         gmsh.model.geo.synchronize()
 
         fragment_list = [(x[0],x[1]) for x in fragment_list]
+        #See here: https://gitlab.onelab.info/gmsh/gmsh/-/blob/master/tutorials/python/t16.py for details on the mapping...
         chip, chip_map = gmsh.model.occ.fragment([(3,chip_base)], fragment_list, removeObject=True, removeTool=True)
+        surface_remappings = {}
+        for e in zip([(3,chip_base)] + fragment_list, chip_map):
+            # print("parent " + str(e[0]) + " -> child " + str(e[1]))
+            surface_remappings[e[0]] = e[1]
+
+        gmsh.model.occ.synchronize()
+        gmsh.model.geo.synchronize()
+
         #Create a physical group for each metal in the design. This is important for capacitance simulations
         for i,metal in enumerate(metal_list):
             metal_name = 'metal_' + str(i)
-            metal_physical_group = gmsh.model.addPhysicalGroup(2, [metal[1]], name = metal_name)
+            cur_metal_surfaces = surface_remappings[metal]
+            metal_physical_group = gmsh.model.addPhysicalGroup(2, [x[1] for x in cur_metal_surfaces], name = metal_name)
             metal_cap_physical_group.append(metal_physical_group)
             metal_cap_names.append(metal_name)
+
+        gmsh.model.occ.synchronize()
+        gmsh.model.geo.synchronize()
+
+        #Create a physical group for each port/junction simulation construct...
+        cur_port_names = [x for x in lePortPolys]
+        for cur_port in cur_port_names:
+            cur_port_surfaces = [(2,x) for x in lePortPolys[cur_port]]
+            cur_port_surfaces_final = []
+            for cur_port_surface in cur_port_surfaces:
+                cur_port_surfaces_final += surface_remappings[cur_port_surface]
+            lePortPolys[cur_port] = gmsh.model.addPhysicalGroup(2, [x[1] for x in cur_port_surfaces_final], name = cur_port)
+
+        gmsh.model.occ.synchronize()
+        gmsh.model.geo.synchronize()
 
         #Add physical group for dielectric gap list
         leDielectricGaps = gmsh.model.addPhysicalGroup(2, [x[1] for x in dielectric_gap_list], name = 'dielectric_gaps')
@@ -144,11 +166,14 @@ class GMSH_Geometry_Builder:
         gmsh.model.occ.synchronize()
         gmsh.model.geo.synchronize()
 
+        #@DavidSommers - basically the mesh elements must be conformal. Thus, as the fragments have been done with
+        #surface metals onto the substrate, this needs to be done onto the air-box as well...
+        #
         #Add in physical groups for each part of the device
-        leDielectric = gmsh.model.addPhysicalGroup(3, [chip_base], name = 'dielectric_substrate')
-
+        leDielectric = gmsh.model.addPhysicalGroup(3, [x[1] for x in chip if x[0] == 3], name = 'dielectric_substrate') #Only taking 3D elements from chip...
+        #
         #Fragment the dielectric volume with the airbox
-        chip_and_air_box, chip_and_air_box_map = gmsh.model.occ.fragment([(3,chip_base)], [(3, air_box)], removeObject=True, removeTool=True)
+        chip_and_air_box, chip_and_air_box_map = gmsh.model.occ.fragment(chip, [(3, air_box)], removeObject=True, removeTool=True)
         
         gmsh.model.occ.synchronize()
         gmsh.model.geo.synchronize()
@@ -261,55 +286,29 @@ class GMSH_Geometry_Builder:
             ground_plane_poly = shapely.difference(metal_surface, shapely.geometry.multipolygon.MultiPolygon(space_polys))
         #
         #Gather all polygons into contiguous groups
-        metal_polys = {}
-        num_polys = 0
+        metal_polys = []
         if not ground_plane['omit'] and not ground_plane_poly.is_empty:
-            metal_polys[0] = ground_plane_poly
-            num_polys += 1
+            metal_polys.append(ground_plane_poly)
         for cur_layer in metallic_layers:
             if cur_layer['type'] == 'design_layer':
                 cur_layer['unit_conv'] = unit_conv
                 cur_layer['resolution'] = self.fillet_resolution
                 cur_layer['threshold'] = threshold
                 metal_polys_all, metal_sel_ids = QUtilities.get_metals_in_layer(self.design, **cur_layer)
-                unique_ids = np.unique(metal_sel_ids)
-                for m in range(unique_ids.size):
-                    metal_polys[m+num_polys] = []
-                for m in range(len(metal_polys_all)):
-                    metal_polys[metal_sel_ids[m]+num_polys] += [metal_polys_all[m]]
-                for m in range(unique_ids.size):
-                    metal_polys[m+num_polys] = shapely.geometry.multipolygon.MultiPolygon(metal_polys[m+num_polys])
-                num_polys += unique_ids.size
+                metal_polys += metal_polys_all
             elif cur_layer['type'] == 'Uclip':
-                if cur_layer['clip_type'] == 'inplane':
+                if cur_layer['clip_type'] == 'inplaneLauncher':
                     Uclip = QUtilities.get_RFport_CPW_groundU_Launcher_inplane(self.design, cur_layer['qObjName'], cur_layer['thickness_side'], cur_layer['thickness_back'], cur_layer['separation_gap'], cur_layer['unit_conv_extra']*1e3) #TODO: Stuck in mm?!
-                metal_polys[num_polys] = shapely.Polygon(Uclip)
-                num_polys += 1
+                elif cur_layer['clip_type'] == 'inplaneRoute':
+                    Uclip = QUtilities.get_RFport_CPW_groundU_Route_inplane(self.design, cur_layer['route_name'], cur_layer['pin_name'], cur_layer['thickness_side'], cur_layer['thickness_back'], cur_layer['separation_gap'], cur_layer['unit_conv_extra']*1e3) #TODO: Stuck in mm?!
+                metal_polys.append(shapely.Polygon(Uclip))
+        #Try to fuse any contiguous polygons...
+        metal_polys = ShapelyEx.fuse_polygons_threshold(metal_polys)
         #If there are any MultiPolygons, convert them into normal polygons...
-        metal_polys_new = []
-        for poly in metal_polys:
-            metal_polys_new += ShapelyEx.shapely_to_list(metal_polys[poly])
-        #Fuse all contiguous polygons into single groups
-        def get_poly_cardinality(poly):
-            if isinstance(poly, shapely.geometry.multipolygon.MultiPolygon):
-                return len(poly.geoms)
-            else:
-                return 1
-        new_polys = [metal_polys_new[0]]
-        for m in range(1, len(metal_polys_new)):
-            found_match = False
-            for n in range(len(new_polys)):
-                cur_check = ShapelyEx.fuse_polygons_threshold([metal_polys_new[m], new_polys[n]], fuse_threshold)
-                if get_poly_cardinality(cur_check) < get_poly_cardinality(metal_polys_new[m]) + get_poly_cardinality(new_polys[n]):
-                    new_polys[n] = cur_check
-                    found_match = True
-                    break
-            if not found_match:
-                new_polys.append(metal_polys_new[m])
+        new_polys = ShapelyEx.shapely_to_list(metal_polys)
 
-        all_metals = shapely.unary_union(new_polys)
         substrate = ShapelyEx.rectangle(self.center_x-self.size_x/2, self.center_y-self.size_y/2, self.center_x+self.size_x/2, self.center_y+self.size_y/2)
-        dielectric_gaps = shapely.difference(substrate, all_metals)
+        dielectric_gaps = shapely.difference(substrate, metal_polys)
         dielectric_gaps = ShapelyEx.shapely_to_list(dielectric_gaps)
 
         return new_polys, dielectric_gaps
@@ -319,7 +318,7 @@ class GMSH_Geometry_Builder:
         for m,poly in enumerate(polygons):
             poly_simplified = poly.simplify(1e-9) #this removes points that are spaced too closely together - TODO: investigate why this is still required?
             cur_coords = poly_simplified.exterior.coords[:-1]
-            if len(cur_coords) == 0:
+            if len(cur_coords) == 0 or poly_simplified.area < 1e-18:
                 continue
             gmsh_exterior = self._draw_polygon_in_GMSH_from_coords(cur_coords) #remove last coord from list because it's repeated
             if len(poly_simplified.interiors) >= 1:
