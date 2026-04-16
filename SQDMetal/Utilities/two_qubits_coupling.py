c3eac57070f701d0cb8e00ef911f4c819266b64f
Leonthi <148446911+Leonthi@users.noreply.github.com>
2026-04-16 17:07:26 +1000

    Updates on two_qubit_coupling function and new fluxline class: L_flux line with cutout (#55)

--- SQDMetal/Utilities/two_qubits_coupling.py
+++ SQDMetal/Utilities/two_qubits_coupling.py
@@ -175,18 +175,19 @@ def two_qubits_coupling(
     )
      
     surname = '' 
+    qubit1_exist = False
     if qubit1_exist:
         surname = '_2nd'
 
     ################# Transmons defintion ###################################################################################
 
-    TransmonTaperedInsets(design, f'Qubit1{surname}', options = Dict(pos_x = f'{pos_x1}mm', pos_y = f'{pos_y1}mm', orientation = q_orient,
+    TransmonTaperedInsets(design, f'Q5{surname}', options = Dict(pos_x = f'{pos_x1}mm', pos_y = f'{pos_y1}mm', orientation = q_orient,
                                                        pocket_lower_tighten = '-100.0um',pocket_height = '500um', pocket_width = '1000um', chrgln_pin_x_offset=f"{chrgln_pin_x_offset}um", chrgln_pin_y_offset=f"{chrgln_pin_y_offset}um",
                                                        taper_width_base = f"{taper_width_base}um", junction_centered = junction_centered_left,
                                                        **readout_pad_options))
 
 
-    TransmonTaperedInsets(design, f'Qubit2{surname}', options = Dict(pos_x = f'{pos_x2}mm', pos_y = f'{pos_y2}mm', orientation = q_orient,
+    TransmonTaperedInsets(design, f'Q4{surname}', options = Dict(pos_x = f'{pos_x2}mm', pos_y = f'{pos_y2}mm', orientation = q_orient,
                                                       pocket_lower_tighten = '-100.0um',pocket_height = '500um', pocket_width = '1000um', chrgln_pin_x_offset=f"{chrgln_pin_x_offset}um", chrgln_pin_y_offset=f"{chrgln_pin_y_offset}um",
                                                       taper_width_base = f"{taper_width_base}um", junction_centered = junction_centered_right,
                                                        **readout_pad_options))
@@ -202,11 +203,11 @@ def two_qubits_coupling(
            rect_length = (-0.041 + np.sqrt(0.041**2 - 4*5.66e-4*(-0.251-J12_target)))/(2*5.66e-4)
 
 
-        Smooth_rectangle(design, f'Capacitor1{surname}',options=Dict(pin_inputs=Dict(start_pin=Dict(component=f'Qubit1{surname}',pin='top_right_pin')), orientation= 180,
+        Smooth_rectangle(design, f'Capacitor1{surname}',options=Dict(pin_inputs=Dict(start_pin=Dict(component=f'Q5{surname}',pin='top_right_pin')), orientation= 180,
                                                               rect_width = f'{rect_width}um',rect_length = f'{rect_length}um', gap_side = f'{gap_side}um', gap_back = f'{gap_back}um', gap_front = f'{gap_front}um',
                                                               fillet_radius = f'{fillet_radius}um',fillet_resolution = 20, gap_fillet_radius = f'{fillet_radius}um'))
 
-        Smooth_rectangle(design, f'Capacitor2{surname}',options=Dict(pin_inputs=Dict(start_pin=Dict(component=f'Qubit2{surname}',pin='top_left_pin')), orientation= 0,
+        Smooth_rectangle(design, f'Capacitor2{surname}',options=Dict(pin_inputs=Dict(start_pin=Dict(component=f'Q4{surname}',pin='top_left_pin')), orientation= 0,
                                                               rect_width = f'{rect_width}um',rect_length = f'{rect_length}um',gap_side = f'{gap_side}um', gap_back = f'{gap_back}um', gap_front = f'{gap_front}um',
                                                               fillet_radius = f'{fillet_radius}um',fillet_resolution = 20, gap_fillet_radius = f'{fillet_radius}um'))
 
@@ -224,11 +225,11 @@ def two_qubits_coupling(
             if circle_offset < -10:
                circle_offset = -10
 
-        Smooth_Capacitor_Semicircle(design, f'Capacitor1{surname}',options=Dict(pin_inputs=Dict(start_pin=Dict(component=f'Qubit1{surname}',pin='top_right_pin')), orientation=cap_orient_180,
+        Smooth_Capacitor_Semicircle(design, f'Capacitor1{surname}',options=Dict(pin_inputs=Dict(start_pin=Dict(component=f'Q5{surname}',pin='top_right_pin')), orientation=cap_orient_180,
                                                               rect_width = f'{rect_width}um',rect_length = f'{rect_length}um', semi_radius = f'{semi_radius}um', circle_offset = f'{circle_offset}um',
                                                               fillet_radius = f'{fillet_radius}um',fillet_resolution = 20, gap_side = f'{gap_side}um', gap_back = f'{gap_back}um'))
 
-        Smooth_Capacitor_Semicircle(design, f'Capacitor2{surname}',options=Dict(pin_inputs=Dict(start_pin=Dict(component=f'Qubit2{surname}',pin='top_left_pin')), orientation=cap_orient_0,
+        Smooth_Capacitor_Semicircle(design, f'Capacitor2{surname}',options=Dict(pin_inputs=Dict(start_pin=Dict(component=f'Q4{surname}',pin='top_left_pin')), orientation=cap_orient_0,
                                                               rect_width = f'{rect_width}um',rect_length = f'{rect_length}um', semi_radius = f'{semi_radius}um', circle_offset = f'{circle_offset}um',
                                                               fillet_radius = f'{fillet_radius}um',fillet_resolution = 20, gap_side = f'{gap_side}um', gap_back = f'{gap_back}um'))
 
@@ -248,12 +249,12 @@ def two_qubits_coupling(
         elif diff_freq:
            bulb_scale_y = 1.2
 
-        Smooth_synapse(design, f'Capacitor1{surname}', options = Dict(pin_inputs=Dict(start_pin=Dict(component=f'Qubit1{surname}',pin='top_right_pin')),
+        Smooth_synapse(design, f'Capacitor1{surname}', options = Dict(pin_inputs=Dict(start_pin=Dict(component=f'Q5{surname}',pin='top_right_pin')),
                                pos_x = '0.0mm', pos_y = '0.0mm', orientation = str(cap_orient_0), fillet_resolution = 16, bulb_radius = f"{bulb_radius}um",
                                                    rect_length = f"{rect_length}um", rect_width = f"{rect_width}um", bulb_scale_y = f"{bulb_scale_y}", circle_offset = f"{circle_offset}um",
                                                    gap_side = f"{gap_side}um", gap_front = f"{gap_front}um", fillet_radius = f"{fillet_radius}um", semi_radius = f"{semi_radius}um", big_fillet_radius = f"{big_fillet_radius}um"))
 
-        Smooth_synapse(design, f'Capacitor2{surname}', options = Dict(pin_inputs=Dict(start_pin=Dict(component=f'Qubit2{surname}',pin='top_left_pin')),
+        Smooth_synapse(design, f'Capacitor2{surname}', options = Dict(pin_inputs=Dict(start_pin=Dict(component=f'Q4{surname}',pin='top_left_pin')),
                                pos_x = '0.0mm', pos_y = '0.0mm', orientation = str(cap_orient_180), fillet_resolution = 16, bulb_radius = f"{bulb_radius}um",
                                                    rect_length = f"{rect_length}um", rect_width = f"{rect_width}um", bulb_scale_y = f"{bulb_scale_y}", circle_offset = f"{circle_offset}um",
                                                    gap_side = f"{gap_side}um", gap_front = f"{gap_front}um", fillet_radius = f"{fillet_radius}um", semi_radius = f"{semi_radius}um", big_fillet_radius = f"{big_fillet_radius}um"))
